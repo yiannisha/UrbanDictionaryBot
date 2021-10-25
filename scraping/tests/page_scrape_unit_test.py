8e0e884a970c7fd0269fcdee0bd25771f083fb53
yiannisha <hadjiyiannis.3@gmail.com>
2021-10-25 22:46:08 +0300

    Added test for get_word_defs

--- scraping/tests/page_scrape_unit_test.py
+++ scraping/tests/page_scrape_unit_test.py
@@ -1,22 +1,37 @@
 
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+
 import unittest
 
 from __main__ import page_scrape
 
 class Test(unittest.TestCase):
 
+    def setUp(self):
+        self.test_case = page_scrape.make_request("bruh")
+
     def test_make_request(self):
         """ Tests make_request """
 
         # Test return type
-        test_case = 'bruh'
-        result = page_scrape.make_request(test_case)
+        result = self.test_case
         self.assertEqual(type(result), BeautifulSoup)
 
     def test_check_word(self):
         """ Tests check_word """
 
         # check if right error is raised
-        test_case = page_scrape.make_request("sfgw")
-        self.assertRaises(page_scrape.NoSuchDefinitionException, page_scrape.check_word, test_case, "sfgw")
+        test_case = page_scrape.make_request('sgfw')
+        self.assertRaises(page_scrape.NoSuchDefinitionException, page_scrape.check_word, test_case)
+
+    def test_get_word_defs(self):
+        """ Tests get_word_defs """
+
+        # check if list is returned
+        test_case = page_scrape.get_word_defs(self.test_case)
+        self.assertEqual(type(test_case), list)
+
+        # check if items have correct types
+        for test in test_case:
+            self.assertEqual(type(test), Tag)
