ca6507f91996651ce36b901c3a55fac52d6692cc
yiannisha <hadjiyiannis.3@gmail.com>
2021-10-28 22:21:42 +0300

    Added anotations to some functions in page_srape.py

--- scraping/page_scrape.py
+++ scraping/page_scrape.py
@@ -3,12 +3,16 @@
 
 import requests
 from requests.exceptions import (InvalidURL, ReadTimeout, HTTPError, ConnectTimeout)
+
 from bs4 import BeautifulSoup
+from bs4.element import Tag
+
+Tags = list[Tag]
 
 class NoSuchDefinitionException(Exception):
     """ Raised when there is no definition for passed word.  """
 
-def make_request(word : str):
+def make_request(word : str) -> BeautifulSoup:
     """Make requests and return a BeautifulSoup object of the page."""
 
     LIMIT = 15
@@ -47,12 +51,12 @@ def check_word(soup : BeautifulSoup):
     else:
         return True
 
-def scrape_word_defs(soup : BeautifulSoup):
+def scrape_word_defs(soup : BeautifulSoup) -> Tags:
     """Returns word definitions as bs4.element.Tags"""
 
     return [div for div in soup.find_all('div', {'class' : 'def-panel'})]
 
-def get_word_defs(word : str):
+def get_word_defs(word : str) -> Tags:
     """Wrapper function for the others functions in this module"""
 
     soup = make_request(word)
